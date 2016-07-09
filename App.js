d479df9bd09333f722650e40df3de2795ff06132
Jefferson Teng <jteng@pandora.com>
2016-07-09 16:35:15 -0700

    Add Gender and npm install sass webpack loaders

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Race from './Race';
+import Gender from './Gender';
 import User from './User';
+
+require('./sass/filterView.scss');
 
 // https://en.wikipedia.org/wiki/Race_and_ethnicity_in_the_United_States#Racial_and_ethnic_categories
 const races = {
@@ -11,6 +14,27 @@ const races = {
                 4: "Middle Eastern American"
               };
 
+// http://www.slate.com/blogs/lexicon_valley/2014/02/21/gender_facebook_now_has_56_categories_to_choose_from_including_cisgender.html
+const genders = {
+                  0:  "Agender/Neutrois",
+                  1:  "Androgyne/Androgynous",
+                  2:  "Bigender",
+                  3:  "Cis/Cisgender",
+                  4:  "Female to Male/FTM",
+                  5:  "Gender Fluid",
+                  6:  "Gender Nonconforming/Variant",
+                  7:  "Gender Questioning",
+                  8:  "Genderqueer",
+                  9:  "Intersex",
+                  10: "Male to Female/MTF",
+                  11: "Neither",
+                  12: "Non-Binary",
+                  13: "Other",
+                  14: "Pangender",
+                  15: "Trans/Transgender",
+                  16: "Transsexual",
+                  17: "Two-Spirit",
+                };
 const users = {
                 0: {
                   "name": "Jefferson Teng",
@@ -34,6 +58,12 @@ class App extends React.Component {
       raceComponentList.push(<Race key={id} name={races[id]} />);
     }
 
+    const genderComponentList = [];
+
+    for (var id in genders) {
+      genderComponentList.push(<Gender key={id} name={genders[id]} />);
+    }
+
     const userComponentList = [];
 
     for (var id in users) {
@@ -41,9 +71,16 @@ class App extends React.Component {
     }
 
     return (
-      <div>
-        {raceComponentList}
-        {userComponentList}
+      <div class="filterView">
+        <div class="races">
+          {raceComponentList}
+        </div>
+        <div class="genders">
+          {genderComponentList}
+        </div>
+        <div class="users">
+          {userComponentList}
+        </div>
       </div>
     );
   }
---
+++ Gender.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Gender extends React.Component {
+  render() {
+    return (
+      <div>{this.props.name}</div>
+    );
+  }
+}
+
+export default Gender;
