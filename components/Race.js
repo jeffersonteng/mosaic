d15796e55cb6b4a6056eb1a0133cd2a6fefa8ae5
Jefferson Teng <jteng3@gmail.com>
2016-07-23 13:50:43 -0700

    Toggle menu item colors on click

--- components/Race.js
+++ components/Race.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class Race extends React.Component {
+  constructor() {
+    super();
+    this.state = {selected: false};
+  }
+
   handleClick() {
     console.log("Race clicked");
+    this.setState({
+      selected: !this.state.selected
+    });
   }
 
   render() {
     return (
-      <div className="filter-list-item" onClick={this.handleClick.bind(this)}>{this.props.name}</div>
+      <div className={"filter-list-item " + (this.state.selected ? "selected" : "")}
+           onClick={this.handleClick.bind(this)}>
+        {this.props.name}
+      </div>
     );
   }
 }
