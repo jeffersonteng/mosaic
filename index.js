a50224b9cd96a2147c91adf8699daf22a2db7778
Jefferson Teng <jteng3@gmail.com>
2016-07-31 12:15:06 -0700

    Connect to Postgres through /db endpoint

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var app = express();
+
+//
+var pg = require('pg');
+var client = new pg.Client();
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/db', (req, res) => {
+
+  // connect to our database
+  client.connect(function (err) {
+    if (err) throw err;
+
+    // execute a query on our database
+    client.query('SELECT * FROM foo', function (err, result) {
+      if (err) throw err;
+
+      // disconnect the client
+      client.end(function (err) {
+        if (err) throw err;
+      });
+
+      res.json(result.rows);
+    });
+  });
 });
 
 app.listen(8080);
